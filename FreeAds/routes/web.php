c8036adbdde84b0b3ecc7a76b30a682258f17307
gary <gary@localhost.localdomain>
2020-11-06 18:55:14 +0100

    [TRY] Merge

--- FreeAds/routes/web.php
+++ FreeAds/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Adds;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,12 +12,32 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
 
+*/
 Route::get('/', function () {
     return view('welcome');
 });
-
+/*
+Route::get('/index', function () {
+    return view('adds.index');
+});
+*/
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/index', 'App\Http\Controllers\DescController@index');
+Route::get('/about', 'App\Http\Controllers\DescController@about');
+Route::get('/contact', 'App\Http\Controllers\DescController@contact');
+
+//Route::resource('/posts', 'App\Http\Controllers\PostsController');
+//Route::resource('/index', 'App\Http\Controllers\AddsController');
+//Route::get('/create', 'App\Http\Controllers\AddsController@create');
+  Route::resource('/adds', 'App\Http\Controllers\AddsController');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
